5c96cd6c01a56fe0ed7306ebca2a1b20d63ad15c
Odwe6 <70318944+Odwe6@users.noreply.github.com>
2021-03-28 16:05:51 +0300

    Update read_altitude_from_DJI_meta.py

--- src/read_altitude_from_DJI_meta.py
+++ src/read_altitude_from_DJI_meta.py
@@ -1,4 +1,4 @@
-# Script read RelativeAltitude information from DJI meta data for all the cameras in the active chunk
+# Script read GpsAltitude information from DJI meta data for all the cameras in the active chunk
 # and loads it to the Reference pane instead of the existing data.
 #
 # This is python script for Metashape Pro. Scripts repository: https://github.com/agisoft-llc/metashape-scripts
@@ -12,9 +12,9 @@ if found_major_version != compatible_major_version:
     raise Exception("Incompatible Metashape version: {} != {}".format(found_major_version, compatible_major_version))
 
 
-def read_DJI_relative_altitude():
+def read_DJI_Gps_altitude():
     """
-    Reads DJI/RelativeAltitude information from the image meta-date and writes it to the Reference pane
+    Reads DJI/GpsAltitude information from the image meta-date and writes it to the Reference pane
     """
 
     doc = Metashape.app.document
@@ -27,13 +27,13 @@ def read_DJI_relative_altitude():
     for camera in chunk.cameras:
         if not camera.reference.location:
             continue
-        if ("DJI/RelativeAltitude" in camera.photo.meta.keys()) and camera.reference.location:
-            z = float(camera.photo.meta["DJI/RelativeAltitude"])
+        if ("DJI/GpsAltitude" in camera.photo.meta.keys()) and camera.reference.location:
+            z = float(camera.photo.meta["DJI/GpsAltitude"])
             camera.reference.location = (camera.reference.location.x, camera.reference.location.y, z)
 
     print("Script finished!")
 
 
-label = "Custom menu/Read RelativeAltitude from DJI metadata"
-Metashape.app.addMenuItem(label, read_DJI_relative_altitude)
+label = "Custom menu/Read GpsAltitude from DJI metadata"
+Metashape.app.addMenuItem(label, read_DJI_Gps_altitude)
 print("To execute this script press {}".format(label))
